ec46c74583fe5c1baeeb4d2c9f1ca6defa2fc866
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2023-09-08 16:06:57 +0900

    Init commit

---
+++ Study/main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Study.BaekJoon;
+
+namespace Study
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            // 백준 코드에 알맞은 Solve() 함수를 넣으면 됩니다.
+
+            Problem24511Solver.Solve();
+        }
+    }
+}
