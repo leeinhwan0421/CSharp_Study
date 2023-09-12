3a17bb9406f348375c019e6ab3bb78fe800c1355
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2023-09-12 11:24:55 +0900

    Update Baekjoon problem

---
+++ Study/BaekJoon/Problem1806Solver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.BaekJoon
+{
+    // https://www.acmicpc.net/problem/1806
+    // 부분합
+
+    internal class Problem1806Solver
+    {
+        static public void Solve()
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            int n = int.Parse(inputs[0]);
+            int s = int.Parse(inputs[1]);
+
+            inputs = Console.ReadLine().Split(' ');
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(inputs[i]);
+            }
+
+            int startIndex = 0, endIndex = 0;
+            int min_len = n + 1;
+            int sum = arr[0];
+
+            while(true)
+            {
+                if (sum < s)
+                {
+                    endIndex++;
+                    if (endIndex == n)
+                        break;
+
+                    sum += arr[endIndex];
+                }
+                else
+                {
+                    sum -= arr[startIndex];
+                    min_len = Math.Min(min_len, endIndex - startIndex + 1);
+                    startIndex++;
+                }
+            }
+
+            Console.Write(min_len == n + 1 ? 0 : min_len);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Study/BaekJoon/Problem3273Solver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.BaekJoon
+{
+    // https://www.acmicpc.net/problem/3273
+    // 두 수의 합
+
+    internal class Problem3273Solver
+    {
+        static public void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] inputs = Console.ReadLine().Split(' ');
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(inputs[i]);
+            }
+
+            Array.Sort(arr);
+
+            int x = int.Parse(Console.ReadLine());
+            int result = 0;
+            int sum = 0;
+            int startIndex = 0;
+            int endIndex = n - 1;
+
+            while(startIndex != endIndex)
+            {
+                sum = arr[startIndex] + arr[endIndex];
+
+                if (sum == x)
+                {
+                    startIndex++;
+                    result++;
+                }
+                else if (sum > x)
+                {
+                    endIndex--;
+                }
+                else
+                {
+                    startIndex++;
+                }
+            }
+
+            Console.Write(result);
+            Console.ReadLine();
+        }
+    }
+}
