b9505eb72040101de5f515f606df43d4463ad30e
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2023-10-05 17:50:04 +0900

    Update Baekjoon Problem

---
+++ Study/BaekJoon/0~10000/Problem1074Solver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.BaekJoon
+{
+    // https://www.acmicpc.net/problem/1074
+    // Z
+
+    internal class Problem1074Solver
+    {
+        static public void Solve()
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            int n = int.Parse(inputs[0]);
+            int r = int.Parse(inputs[1]);
+            int c = int.Parse(inputs[2]);
+
+            int result = 0;
+            while(n != 0)
+            {
+                n -= 1;
+
+                if (r < Math.Pow(2, n) && c < Math.Pow(2, n))
+                {
+                }
+                else if (r < Math.Pow(2, n) && c >= Math.Pow(2, n))
+                {
+                    result += (int)Math.Pow(2, n) * (int)Math.Pow(2, n);
+                    c -= (int)Math.Pow(2, n);
+                }
+                else if (r >= Math.Pow(2, n) && c < Math.Pow(2, n))
+                {
+                    result += (int)Math.Pow(2, n) * (int)Math.Pow(2, n) * 2;
+                    r -= (int)Math.Pow(2, n);
+                }
+                else
+                {
+                    result += (int)Math.Pow(2, n) * (int)Math.Pow(2, n) * 3;
+                    r -= (int)Math.Pow(2, n);
+                    c -= (int)Math.Pow(2, n);
+                }
+            }
+
+            Console.Write(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Study/BaekJoon/0~10000/Problem1697Solver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Study.BaekJoon
+{
+    internal class Problem1697Solver
+    {
+        static int max = 100001;
+        static int n, k;
+
+        static bool[] visited = new bool[max];
+        static int[] path = new int[max];
+
+        public static void Solve()
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            n = int.Parse(inputs[0]);
+            k = int.Parse(inputs[1]);
+
+            if (n == k)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            Console.WriteLine(BFS(n));
+            Console.ReadLine();
+        }
+
+        static int BFS(int v)
+        {
+            visited[v] = true;
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(v);
+
+            while (queue.Count > 0)
+            {
+                int current = queue.Dequeue();
+                if (current == k)
+                {
+                    return path[current];
+                }
+
+                if (current - 1 >= 0 && !visited[current - 1])
+                {
+                    visited[current - 1] = true;
+                    path[current - 1] = path[current] + 1;
+                    queue.Enqueue(current - 1);
+                }
+
+                if (current + 1 < max && !visited[current + 1])
+                {
+                    visited[current + 1] = true;
+                    path[current + 1] = path[current] + 1;
+                    queue.Enqueue(current + 1);
+                }
+
+                if (current * 2 < max && !visited[current * 2])
+                {
+                    visited[current * 2] = true;
+                    path[current * 2] = path[current] + 1;
+                    queue.Enqueue(current * 2);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
+
