4265537c7ec265045e0362c1f2a43e55151f1906
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2024-08-29 17:57:05 +0900

    Update Notepad.cs

--- Study/Notepad/Notepad.cs
+++ Study/Notepad/Notepad.cs
@@ -10,6 +10,31 @@ namespace Study.Notepad
     {
         static public void Run()
         {
+            string[] line = Console.ReadLine().Split('-');
+
+            int sum = Plus(line[0]);
+
+            for (int i = 1; i < line.Length; i++)
+            {
+                sum -= Plus(line[i]);
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+        static public int Plus(string n)
+        {
+            string[] line = n.Split('+');
+
+            int sum = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                sum += int.Parse(line[i]);
+            }
+
+            return sum;
         }
     }
 }
