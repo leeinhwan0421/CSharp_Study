43e0d41eb542724189dbe6846d6020abc9e653b5
이인환 <70061182+leeinhwan0421@users.noreply.github.com>
2024-09-02 19:15:36 +0900

    Create Problem1264Solver.cs

---
+++ Study/BaekJoon/0~10000/Problem1264Solver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.BaekJoon
+{
+    // https://www.acmicpc.net/problem/1264
+    // 모음
+
+    internal class Problem1264Solver
+    {
+        static public void Solve()
+        {
+            while (true)
+            {
+                string it = Console.ReadLine();
+
+                if (it == "#")
+                    return;
+
+                it = it.ToLower();
+
+                int cnt = 0;
+                for (int i = 0; i < it.Length; i++)
+                {
+                    if (it[i] == 'a' || it[i] == 'e' || it[i] == 'i' || it[i] == 'o' || it[i] == 'u')
+                        cnt++;
+                }
+
+                Console.WriteLine(cnt);
+            }
+        }
+    }
+}
