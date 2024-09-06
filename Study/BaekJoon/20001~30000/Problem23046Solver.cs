44ed7103a49e796133f01a11e1a258000837d002
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2024-09-06 17:49:13 +0900

    update problems

---
+++ Study/BaekJoon/20001~30000/Problem23046Solver.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using System.Numerics;
+using System.Linq;
+
+namespace Study.BaekJoon
+{
+    // https://www.acmicpc.net/problem/23046
+    // 큰 수 뒤집기 
+
+    class Problem23046Solver
+    {
+        private class DQ
+        {
+            public int[] arr;
+            public int s, e;
+            public int updateCnt;
+
+            public DQ(int n, int first)
+            {
+                arr = new int[n * 2 + 1];
+                s = e = n;
+                arr[s] = first;
+                updateCnt = 1;
+            }
+
+            public void AddFirst(int n)
+            {
+                arr[--s] = n;
+                updateCnt++;
+            }
+
+            public void AddLast(int n)
+            {
+                arr[++e] = n;
+                updateCnt++;
+            }
+
+            public void SetUpdateCntZero()
+            {
+                updateCnt = 0;
+            }
+
+            public bool NeedUpdate()
+            {
+                return updateCnt != 0;
+            }
+        }
+
+        private static int[] x;
+        private static DQ s;
+        private static bool isReverse = false;
+
+        private static void AddToX()
+        {
+            if (!s.NeedUpdate()) return;
+
+            int updateCnt = s.updateCnt;
+            s.SetUpdateCntZero();
+
+            int len = s.e - s.s + 1;
+            int[] tmp = new int[len];
+            int[] tmp2 = new int[len];
+
+            if (isReverse)
+            {
+                for (int i = s.s, tmpIdx1 = tmp.Length - 1; i <= s.e; i++, tmpIdx1--)
+                {
+                    tmp[tmpIdx1] = s.arr[i];
+                    tmp2[tmpIdx1] = s.arr[i];
+                }
+            }
+            else
+            {
+                for (int i = s.e, tmpIdx2 = tmp.Length - 1; i >= s.s; i--, tmpIdx2--)
+                {
+                    tmp[tmpIdx2] = s.arr[i];
+                    tmp2[tmpIdx2] = s.arr[i];
+                }
+            }
+
+            for (int i = 1; i < tmp.Length; i++)
+            {
+                tmp[i] += tmp2[i - 1];
+                tmp2[i] += tmp2[i - 1];
+                if (i - updateCnt >= 0)
+                    tmp[i] -= tmp2[i - updateCnt];
+            }
+
+            int idx = x.Length - 1;
+            int tmpIdx = tmp.Length - 1;
+            while (len-- > 0)
+            {
+                x[idx--] += tmp[tmpIdx--];
+            }
+        }
+
+        public static void Solve()
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            {
+                string input = sr.ReadLine();
+
+                x = new int[input.Length + 2];
+                s = new DQ(input.Length, input[0] - '0');
+
+                int cnt = 0;
+                for (int i = 1; i < input.Length; i++)
+                {
+                    char c = input[i];
+                    if (c == '-')
+                    {
+                        AddToX();
+                        isReverse = !isReverse;
+                        continue;
+                    }
+
+                    cnt++;
+                    if (!isReverse) s.AddLast(c - '0');
+                    else s.AddFirst(c - '0');
+                }
+                AddToX();
+
+                for (int i = x.Length - 1; i > input.Length + 1 - cnt; i--)
+                {
+                    if (x[i] >= 10)
+                    {
+                        x[i - 1] += x[i] / 10;
+                        x[i] %= 10;
+                    }
+                }
+
+                StringBuilder sb = new StringBuilder();
+                int startIdx = input.Length + 1 - cnt;
+                for (int i = startIdx; i < x.Length; i++)
+                {
+                    if (x[i] != 0) break;
+                    startIdx++;
+                }
+                for (int i = startIdx; i < x.Length; i++)
+                {
+                    sb.Append(x[i]);
+                }
+
+                Console.WriteLine(sb.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Study/BaekJoon/20001~30000/Problem25682Solver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.BaekJoon
+{
+    // https://www.acmicpc.net/problem/25682
+    // 체스판 다시 칠하기 2
+
+    internal class Problem25682Solver
+    {
+        static public void Solve()
+        {
+            int rows, cols, kSize, minTotal = 2000001;
+            int[,] prefixSum = new int[2001, 2001];
+
+            string[] input = Console.ReadLine().Split();
+            rows = int.Parse(input[0]);
+            cols = int.Parse(input[1]);
+            kSize = int.Parse(input[2]);
+
+            for (int i = 1; i <= rows; i++)
+            {
+                string line = Console.ReadLine();
+                for (int j = 1; j <= cols; j++)
+                {
+                    char color = line[j - 1];
+                    if (((i + j) % 2 == 0 && color != 'B') || ((i + j) % 2 != 0 && color == 'B'))
+                        prefixSum[i, j] = 1;
+                    prefixSum[i, j] += prefixSum[i - 1, j] + prefixSum[i, j - 1] - prefixSum[i - 1, j - 1];
+                }
+            }
+
+            for (int i = kSize; i <= rows; i++)
+            {
+                for (int j = kSize; j <= cols; j++)
+                {
+                    int countPainting = prefixSum[i, j] - prefixSum[i - kSize, j] - prefixSum[i, j - kSize] + prefixSum[i - kSize, j - kSize];
+                    countPainting = Math.Min(kSize * kSize - countPainting, countPainting);
+                    minTotal = Math.Min(minTotal, countPainting);
+                }
+            }
+
+            Console.WriteLine(minTotal);
+            Console.ReadKey();
+        }
+    }
+}
