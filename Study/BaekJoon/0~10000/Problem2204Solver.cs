31cce4c3de659dad26afd1b403ba3a2af4785d2e
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2024-10-28 17:51:51 +0900

    add problem

---
+++ Study/BaekJoon/0~10000/Problem2204Solver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.BaekJoon
+{
+    // https://www.acmicpc.net/problem/2204
+    // 도비의 난독증 테스트 
+
+    internal class Problem2204Solver
+    {
+        static public void Solve()
+        {
+            while (true)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                if (n == 0)
+                {
+                    break;
+                }
+
+                List<string> list = new List<string>();
+
+                for (int i = 0; i < n; i++)
+                {
+                    list.Add(Console.ReadLine());
+                }
+
+                list.Sort(StringComparer.OrdinalIgnoreCase);
+
+                Console.WriteLine(list.First());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- Study/main.cs
+++ Study/main.cs
@@ -15,7 +15,7 @@ namespace Study
         static void Main(string[] args)
         {
             // 백준 코드에 알맞은 Solve() 함수를 넣으면 됩니다.
-            Problem16435Solver.Solve();
+            Problem2204Solver.Solve();
 
             // Sort 집어넣기
             // Count_sort.Sort();
