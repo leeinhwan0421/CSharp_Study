988b2d02ee035e8e281b36a7a4ca5bd581a162ca
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2024-11-06 13:35:21 +0900

    add problem

---
+++ Study/BaekJoon/30001~40000/Problem31831Solver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Study.BaekJoon
+{
+    // https://www.acmicpc.net/problem/31831
+    // 과민성 대장 증후군
+
+    class Problem31831Solver
+    {
+        static public void Solve()
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            int N = int.Parse(inputs[0]);
+            int M = int.Parse(inputs[1]);
+
+            string[] A = Console.ReadLine().Split(' ');
+            int delta = 0;
+            int result = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                delta += int.Parse(A[i]);
+
+                if (delta < 0)
+                {
+                    delta = 0;
+                }
+
+                if (delta >= M)
+                {
+                    result++;
+                }
+            }
+
+            Console.Write(result);
+        }
+    }
+}
