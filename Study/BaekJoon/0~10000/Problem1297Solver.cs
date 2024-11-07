680ffd3c7b6d5536ab9f012d2e271619a33864fa
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2024-11-07 17:39:14 +0900

    add problem

---
+++ Study/BaekJoon/0~10000/Problem1297Solver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.BaekJoon
+{
+    // https://www.acmicpc.net/problem/1297
+    // TV 크기
+
+    internal class Problem1297Solver
+    {
+        static public void Solve()
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            int D = int.Parse(inputs[0]);
+            int H = int.Parse(inputs[1]);
+            int W = int.Parse(inputs[2]);
+
+            double DIAGONAL = Math.Sqrt(W * W + H * H);
+            double width = D * W / DIAGONAL;
+            double height = D * H / DIAGONAL;
+
+            Console.Write($"{(int)height} {(int)width}");
+        }
+    }
+}
